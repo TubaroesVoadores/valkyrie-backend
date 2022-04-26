a98f9e3d0dc0de2379dae6ca7c23eb4971725316
PedroLuzBGC <pedro.henrique@bgcbrasil.com.br>
2022-04-25 21:09:20 -0300

    fix: :bug: fix in docs

--- src/functions/projects/createProject.js
+++ src/functions/projects/createProject.js
@@ -11,7 +11,7 @@ import { Projects } from '../../models';
  * @param {Object} event - Base event object of AWS Lambda.
  * @param {Object} event.body.name - Project's name.
  * @param {Object} event.requestContext.identity.cognitoIdentityId - User's id of cognito.
- * @command sls invoke local -f CreateProject -p tests/mocks/Projects/createProject.json
+ * @command sls invoke local -f CreateProject -p tests/mocks/Projects/createProject.json  -s STAGE
  */
 
 export const main = async (event) => {
