0d4dac7ee6307a8095980f2892a84dee5fc95898
PedroLuzBGC <pedro.henrique@bgcbrasil.com.br>
2022-04-30 16:05:33 -0300

    feat: :sparkles: get project created

---
+++ src/functions/projects/getProject.js
@@ -0,0 +1,58 @@
+import {
+  apiError,
+  apiResponse,
+  getEventParams,
+  NotFoundError,
+} from '../../utils';
+import { Images, Projects } from '../../models';
+
+/**
+ * @name GetProject
+ * @description This api is responsible for deleting a project.
+ * @param {Object} event - Base event object of AWS Lambda.
+ * @param {Object} event.pathParams.projectId - Project's name.
+ * @command sls invoke local -f GetProject -p tests/mocks/Projects/getProject.json -s STAGE
+ */
+
+export const main = async (event) => {
+  try {
+    const { projectId } = getEventParams(event);
+
+    const [project] = await Projects
+      .query('id')
+      .eq(projectId)
+      .where('deletedAt')
+      .not()
+      .exists()
+      .exec();
+
+    if (!project) throw new NotFoundError('Project not found!');
+
+    const images = (
+      await Images
+        .query('projectId')
+        .eq(projectId)
+        .where('deletedAt')
+        .not()
+        .exists()
+        .exec()
+    ).toJSON();
+
+    const response = {
+      ...project,
+      images,
+    };
+
+    console.dir({ response }, { depth: 1000 });
+
+    return apiResponse({
+      message: 'Project deleted!',
+      project: {
+        ...project,
+        images,
+      },
+    }, 200);
+  } catch (error) {
+    return apiError(error);
+  }
+};
