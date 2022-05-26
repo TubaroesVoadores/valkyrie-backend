e08d15aaaec6e28cd94c6335c60362e0dc46b851
PedroLuzBGC <pedro.henrique@bgcbrasil.com.br>
2022-05-25 21:54:45 -0300

    fix

--- src/utils/getEventParams.js
+++ src/utils/getEventParams.js
@@ -5,15 +5,14 @@ export const getEventParams = (event) => {
     requestContext,
   } = event;
 
-  console.log('body', { event: body });
   const cognitoIdentityId = requestContext?.identity?.cognitoIdentityId;
   const entry = typeof body === 'string' ? JSON.parse(body) : body;
 
-  console.log('Entry:', { entry });
+  console.log('Entry: ', { entry });
 
   return {
     ...entry,
     ...pathParameters,
-    userId: cognitoIdentityId,
+    userId: entry?.userId || cognitoIdentityId,
   };
 };
