cbe66d700912d81b7caf0582dcacbcbc6bc8f27a
PedroLuzBGC <pedro.henrique@bgcbrasil.com.br>
2022-05-29 18:44:33 -0300

    feat: :sparkles: create libs mocks

---
+++ src/models/__mocks__/dynamoose.js
@@ -0,0 +1,37 @@
+export class Model {
+  constructor(attributes = {}) {
+    this.setData(attributes);
+  }
+
+  setData(props) {
+    for (const [key, value] of Object.entries(props)) {
+      if (value !== '' && value !== null && value !== undefined) {
+          this[key] = value;
+      }
+    }
+  }
+
+  static query() {
+    return this;
+  }
+
+  static exists() {
+    return this;
+  }
+
+  static where() {
+    return this;
+  }
+
+  static eq() {
+    return this;
+  }
+
+  static and() {
+    return this;
+  }
+
+  static exec() {
+    return [];
+  }
+}
